a113cac5c3befae3c03471364ff1465b15ea1ce6
Ilya Nesterov <worknesterov@yandex.kz>
2025-06-08 21:56:53 +0500

    fix: refactored vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint(), svgr()],
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), svgr(), mode !== 'production' && eslint()].filter(Boolean),
+}));
